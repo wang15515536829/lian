8646db90112e503b2802621f7d9ff70947ee548b
mutouren <994217300@qq.com>
2015-06-26 11:49:32 +0800

    6.26.12王鹏

---
+++ LianMeng/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  LianMeng
+//
+//  Created by lanouhn on 15/6/26.
+//  Copyright (c) 2015年 lanouhn3g - 23 .com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+
+@end
---
+++ LianMeng/SetViewController.m
@@ -0,0 +1,37 @@
+//
+//  SetViewController.m
+//  LianMeng
+//
+//  Created by lanouhn on 15/6/26.
+//  Copyright (c) 2015年 lanouhn3g - 23 .com. All rights reserved.
+//
+
+#import "SetViewController.h"
+
+@interface SetViewController ()
+
+@end
+
+@implementation SetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
